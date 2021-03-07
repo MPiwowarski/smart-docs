9f88dc6c753369e6e26425055c94a6ebd29980e5
Marcin Piwowarski <marcpiwowarski@hotmail.com>
2021-03-07 23:27:22 +0000

    Transaction entity created

---
+++ SmartDocs/SmartDocs/Entities/Transaction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartDocs.Entities
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public string TransactionAddress { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+    }
+}
